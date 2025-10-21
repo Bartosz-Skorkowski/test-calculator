90ce5cd682ab55d0db43f186e97c6d9b653d7457
Bartosz-Skorkowski <skorkowskibartek@gmail.com>
2025-10-21 13:15:14 +0200

    Dadanie form napiwkow

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// function calculateTip() {
-//     // Kod do implementacji
-// }
+const form = document.getElementById("formularz");
+form.addEventListener("submit", calculateTip);
 
-// ... Kod do implementacji
+function calculateTip(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const kwota = parseFloat(data.get("kwota"));
+
+  const napiwek = parseFloat(data.get("Napiwek"));
+
+  const procent = napiwek / 100;
+
+  let calosc = kwota + procent * kwota;
+
+  let wynik = document.getElementById("wynik");
+  let nap = document.getElementById("nap");
+  nap.innerText = "Napiwek: " + kwota * procent;
+
+  wynik.innerText = `Kwota napiwku: ${
+    kwota * napiwek
+  }, Kwota całkowita: ${calosc}`;
+}
